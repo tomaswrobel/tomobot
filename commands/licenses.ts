241536428609dd5a01a8f7bb1353a5dcf6b4e34e
Tomáš Wróbel <coder@tomaswrobel.dev>
2023-06-12 08:08:59 +0000

    Improving Licenses command

--- commands/licenses.ts
+++ commands/licenses.ts
@@ -1,9 +1,13 @@
 import {EmbedBuilder} from "discord.js";
-import {dependencies} from "../package.json";
+import {dependencies, repository} from "../package.json";
 import SlashCommand from "../src/SlashCommand";
 import {join} from "path";
 
+const api = `https://api.github.com/repos${new URL(repository).pathname}`;
+
 export = new SlashCommand({description: "See open-source licenses"}, async function* () {
+	yield SlashCommand.DEFER;
+
 	const embed = new EmbedBuilder();
 
 	for (const name in dependencies) {
@@ -11,19 +15,28 @@ export = new SlashCommand({description: "See open-source licenses"}, async funct
 
 		embed.addFields({
 			name,
-			value: `${
-				description || "No description provided."
-			}\n[View on npm](https://npmjs.com/package/${name})`,
+			value: `${description}\n[View on npm](https://npmjs.com/package/${name})`,
 		});
 	}
 
-	embed.addFields({
-		name: "EvoBot",
-		value: "The core of the music functions. \n[View on GitHub](https://github.com/eritislami/evobot)"
-	})
+
+	const {owner, description} = await fetch(api).then(res => res.json());
 
 	yield {
-		content: `**${this.client.user?.username} would not be possible without the following open source projects:**\n\n`,
-		embeds: [embed],
+		content: `**${this.client.user.username} would not be possible without the following open source projects:**\n\n`,
+		embeds: [
+			embed.addFields({
+				name: "evobot",
+				value: "The core of the music functions. \n[View on GitHub](https://github.com/eritislami/evobot)",
+			}),
+			new EmbedBuilder()
+				.setTitle(`${this.client.user.username} source code`)
+				.setAuthor({
+					name: owner.login,
+					iconURL: owner.avatar_url,
+					url: repository
+				})
+				.setDescription(description),
+		],
 	};
 });
